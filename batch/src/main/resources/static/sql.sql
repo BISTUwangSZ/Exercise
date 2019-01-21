3875901d0adb599f7ba02fda966d1a81a5dbbbf2
wangsizhuo <wangsizhuo@btte.net>
2019-01-21 14:32:05 +0800

    增加sql文件

---
+++ batch/src/main/resources/static/sql.sql
@@ -0,0 +1,18 @@
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for a
+-- ----------------------------
+DROP TABLE IF EXISTS `a`;
+CREATE TABLE `a`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `age` int(11) NOT NULL,
+  `gender` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `psw` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
+  `seq` int(11) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 23 CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
